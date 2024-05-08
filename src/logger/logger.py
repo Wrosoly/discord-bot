f3255d87b833f5e69ea07e885acbae5ed9527dcc
Pearoo <sixtyny@gmail.com>
2024-05-08 19:35:43 +0200

    Delete logger as it can be used as a cog

--- src/logger/logger.py
+++
@@ -1,9 +0,0 @@
-import discord
-import discord.ext
-
-class TMLogger():
-    def __init__(self) -> None:
-        pass
-
-    async def handleMessage(bot: discord.ext.commands.Bot, message: discord.Message):
-        print(message)
